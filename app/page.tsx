4d100e6bbba1a9f264ce836984adfbb22c54cadd
BeEmpathic <xdgrzybeknr1xd@gmail.com>
2026-05-17 08:42:02 +0200

    Did more styling version 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,31 @@
 import Image from "next/image";
 
 export default function Home() {
+  // TODOS FOR THIS PROJECT !!!!!!:
+  // - Try making external classes for inputs
+  // - Finish styling:
+  // -  Make it so it looks better
+  // - Make a place to show the error
+  // - Finish styling the button
+  // - Add padding to the inputs
   return (
     <div className="flex margin-auto items-center justify-center bg-background text-foreground w-full min-h-dvh">
-      <form className="inset-shadow-lg shadow-purple-400 rounded-lg bg-[#F0FFC2] flex flex-col p-8 border min-h-1/3 broder-black gap-5">
+      <form className="min-w-lg inset-shadow-xl shadow-purple-400 rounded-2xl bg-[#F0FFC2] flex flex-col p-8 border min-h-1/3 gap-5">
+        <h1 className="text-center font-bold">Welcome Home Master!</h1>
         <div className="flex flex-col">
           <label htmlFor="username">Username: </label>
           <input className="border border-black" type="text" name="username" />
         </div>
-        <div className="border border-black flex flex-col">
+        <div className=" flex flex-col">
           <label htmlFor="password">Password: </label>
-          <input type="password" name="password" />
+          <input
+            className="border border-black"
+            type="password"
+            name="password"
+          />
         </div>
 
-        <button className="bg-[#EAE6BC] p-3 rounded-full border border-black">
+        <button className="text-white bg-[#35858E] p-3 rounded-full border border-black hover:cursor-pointer">
           Login Master!
         </button>
       </form>
